7d8370c98aa8e8b7971a54d6510406aef78df4e4
Kenny Schaefer <kenneth.schaefer0@gmail.com>
2023-02-02 08:05:37 -0600

    Update map and Location pages

--- components/Location.jsx
+++ components/Location.jsx
@@ -2,8 +2,109 @@ import React from "react"
 import Map from "./Map"
 import { View, Text } from "react-native"
 import STYLES from "../assets/Styles"
+import { FlatList } from "react-native"
 
 const Location = () => {
+  const directionsFromMSU = [
+    {
+      key: 1,
+      direction: "Head southwest on Sheppard Access Rd toward Enterprise St"
+    },
+    {
+      key: 2,
+      direction: "Keep left to continue on TX-325 Spur S"
+    },
+    {
+      key: 3,
+      direction: "Take the I-44 W/US-277 S/US-281 S exit toward US-287 S/Wichita Fls/Ft Worth"
+    },
+    {
+      key: 4,
+      direction: "Merge onto I-44/US-277 S/US-281 S"
+    },
+    {
+      key: 5,
+      direction: "Continue onto US-277 S/US-281 S/US-287 S"
+    },
+    {
+      key: 6,
+      direction: "Take the US-82 W/US-277 S exit toward Lubbock/Abilene"
+    },
+    {
+      key: 7,
+      direction: "Continue onto US-277 S/US-82 W"
+    },
+    {
+      key: 8,
+      direction: "Take the exit toward FM-1634/Barnett Rd"
+    },
+    {
+      key: 9,
+      direction: "Merge onto Kell W Blvd"
+    },
+    {
+      key: 10,
+      direction: "Turn left onto Barnett Rd"
+    },
+    {
+      key: 11,
+      direction: "Turn right onto Southwest Pkwy"
+    },
+    {
+      key: 12,
+      direction: "Destination will be on the right"
+    }
+  ]
+  const directionsFromSheppardAFB = [
+    {
+      key: 1,
+      direction: "Head southwest on Sheppard Access Rd toward Enterprise St"
+    },
+    {
+      key: 2,
+      direction: "Keep left to continue on TX-325 Spur S"
+    },
+    {
+      key: 3,
+      direction: "Take the I-44 W/US-277 S/US-281 S exit toward US-287 S/Wichita Fls/Ft Worth"
+    },
+    {
+      key: 4,
+      direction: "Merge onto I-44/US-277 S/US-281 S"
+    },
+    {
+      key: 5,
+      direction: "Continue onto US-277 S/US-281 S/US-287 S"
+    },
+    {
+      key: 6,
+      direction: "Take the US-82 W/US-277 S exit toward Lubbock/Abilene"
+    },
+    {
+      key: 7,
+      direction: "Continue onto US-277 S/US-82 W"
+    },
+    {
+      key: 8,
+      direction: "Take the exit toward FM-1634/Barnett Rd"
+    },
+    {
+      key: 9,
+      direction: "Merge onto Kell W Blvd"
+    },
+    {
+      key: 10,
+      direction: "Turn left onto Barnett Rd"
+    },
+    {
+      key: 11,
+      direction: "Turn right onto Southwest Pkwy"
+    },
+    {
+      key: 12,
+      direction: "Destination will be on the right"
+    }
+  ]
   return (
     <>
       <View style={STYLES.header}>
@@ -16,54 +117,24 @@ const Location = () => {
           <Text>We are located at 5432 Southwest Parkway across the street from Memorial Stadium!</Text>
           <Map />
         </View>
-        <View id='directions'>
-          <View className='directions'>
-            <Text>Directions from MSU</Text>
-            <ol>
-              <li>
-                Head north toward Council Dr
-              </li>
-              <li>
-                Turn left toward Council Dr
-              </li>
-              <li>
-                Turn right at the 1st cross street onto Council Dr
-              </li>
-              <li>
-                Turn left onto Hampstead Ln
-              </li>
-              <li>
-                Slight left to stay on Hampstead Ln
-              </li>
-              <li>
-                Turn left onto Kemp Blvd
-              </li>
-              <li>
-                Turn right onto Southwest Pkwy
-              </li>
-              <li>
-                Destination will be on the right
-              </li>
-            </ol>
+        <View styles={STYLES.directionsContainer}>
+          <View style={STYLES.flatList}>
+            <Text style={STYLES.directionHeader}>Directions from MSU</Text>
+            <FlatList
+              data={directionsFromMSU}
+              renderItem={
+                ({ item }) => (<Text style={STYLES.directionText}> {item.key}. {item.direction}</Text>)
+              }
+            />
           </View>
-          <View className="directions">
-            <Text>Directions from Sheppard AFB</Text>
-            <ol>
-              <li>Head southwest on Sheppard Access Rd toward Enterprise St</li>
-              <li>Keep left to continue on TX-325 Spur S</li>
-              <li>Take the I-44 W/US-277 S/US-281 S exit toward US-287 S/Wichita Fls/Ft Worth</li>
-              <li>Merge onto I-44/US-277 S/US-281 S</li>
-              <li>Continue onto US-277 S/US-281 S/US-287 S</li>
-              <li>Take the US-82 W/US-277 S exit toward Lubbock/Abilene</li>
-              <li>Continue onto US-277 S/US-82 W</li>
-              <li>Take the exit toward FM-1634/Barnett Rd</li>
-              <li>Merge onto Kell W Blvd</li>
-              <li>Turn left onto Barnett Rd</li>
-              <li>Turn right onto Southwest Pkwy</li>
-              <li>
-                Destination will be on the right
-              </li>
-            </ol>
+          <View style={STYLES.flatList}>
+            <Text style={STYLES.directionHeader}>Directions from Sheppard AFB</Text>
+            <FlatList
+              data={directionsFromSheppardAFB}
+              renderItem={
+                ({ item }) => <Text style={STYLES.directionText}> {item.key}. {item.direction}</Text>
+              }
+            />
           </View>
         </View>
       </View>
